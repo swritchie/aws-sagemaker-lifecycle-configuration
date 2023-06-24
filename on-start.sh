064ecc3f39a12cffcf66a3499926f0ab4f2f40e3
Skylar Ritchie <48811639+swritchie@users.noreply.github.com>
2023-06-24 06:54:32 -0700

    Simplify

--- on-start.sh
+++ on-start.sh
@@ -7,15 +7,7 @@ nohup sudo -b -u ec2-user -i <<'EOF'
 
 # Declare constants
 NAME="skylar_env"
-PYTHON=3.10
-
-# Install GitHub CLI
-type -p yum-config-manager >/dev/null || sudo yum install yum-utils -y
-sudo yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo yum install gh -y
-
-# Install DVC 
-pip install dvc
+PYTHON=3.9
 
 # Create environment
 conda create \
@@ -30,25 +22,18 @@ conda activate $NAME
 
 # Install packages
 pip install \
-    catboost \
-    category_encoders \
+    awswrangler \
     composeml \
     docker-compose \
-    fastparquet \
+    evalml \
     feature_engine \
-    featuretools \
     fsspec \
-    python-git \
-    openpyxl \
-    pandas \
     pyyaml \
     s3fs \
     sagemaker \
-    scikit-learn \
     scikit-plot \
     seaborn \
-    tqdm \
-    woodwork
+    tqdm
 
 # Install toolboxes
 pip install git+https://github.com/swritchie/skylar-toolbox.git
